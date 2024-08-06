bc8a8619a4f2009be12cd8549b471efae37ed975
Jaehyeon Park <skystar@skystar.dev>
2024-08-06 18:30:43 +0900

    implement telegram alert

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"time"
+
+	"github.com/caarlos0/env/v11"
+	"github.com/mymmrac/telego"
+	tu "github.com/mymmrac/telego/telegoutil"
 )
 
 var (
@@ -13,6 +18,17 @@ var (
 	GOODSID    = "24011105"
 	PlaySeqCnt = 2
 )
+
+var (
+	config *ConfigStruct
+)
+
+type ConfigStruct struct {
+	TelegramToken  string `env:"TELEGRAM_TOKEN,required"`
+	TelegramChatID int64  `env:"TELEGRAM_CHAT_ID,required"`
+
+	SleepDuration time.Duration `env:"SLEEP_DURATION" envDefault:"5s"`
+}
 
 type SeatResponse struct {
 	Data SeatData `json:"data"`
@@ -30,11 +46,54 @@ type Seat struct {
 }
 
 func main() {
+	config = new(ConfigStruct)
+	if err := env.Parse(config); err != nil {
+		log.Fatal(err)
+	}
+
+	bot, err := telego.NewBot(config.TelegramToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Start checking...\n")
+
+	if _, err := bot.SendMessage(tu.Message(tu.ID(config.TelegramChatID), "Start checking...")); err != nil {
+		log.Printf("Failed to send message: %v\n", err)
+	}
+
+	for {
+		seatData, err := checkSeat()
+		if err != nil {
+			log.Printf("Failed to check seat: %v\n", err)
+			time.Sleep(config.SleepDuration)
+			continue
+		}
+
+		for _, seat := range seatData {
+			for _, s := range seat.RemainSeat {
+				if s.RemainCnt > 0 {
+					msg := fmt.Sprintf("Seat Found!!!: PlaySeq: %s, RemainCnt: %d, SeatGradeName: %s\n", s.PlaySeq, s.RemainCnt, s.SeatGradeName)
+					if _, err := bot.SendMessage(tu.Message(tu.ID(config.TelegramChatID), msg)); err != nil {
+						log.Printf("Failed to send message: %v\n", err)
+					}
+				}
+				fmt.Printf("PlaySeq: %s, RemainCnt: %d, SeatGrade: %s, SeatGradeName: %s\n", s.PlaySeq, s.RemainCnt, s.SeatGrade, s.SeatGradeName)
+			}
+		}
+
+		time.Sleep(config.SleepDuration)
+	}
+
+}
+
+func checkSeat() ([]*SeatData, error) {
+	var seatData []*SeatData
 	for i := 1; i <= PlaySeqCnt; i++ {
 		urlStr := fmt.Sprintf(URL, GOODSID, i)
 		u, err := url.Parse(urlStr)
 		if err != nil {
-			log.Fatal(err)
+			return nil, err
 		}
 
 		headers := http.Header{
@@ -54,17 +113,22 @@ func main() {
 		client := &http.Client{}
 		resp, err := client.Do(req)
 		if err != nil {
-			log.Fatal(err)
+			return nil, err
 		}
 
 		var seatResp SeatResponse
 		if err := json.NewDecoder(resp.Body).Decode(&seatResp); err != nil {
-			log.Fatal(err)
+			return nil, err
 		}
 
-		for _, seat := range seatResp.Data.RemainSeat {
-			fmt.Printf("PlaySeq: %s, RemainCnt: %d, SeatGrade: %s, SeatGradeName: %s\n", seat.PlaySeq, seat.RemainCnt, seat.SeatGrade, seat.SeatGradeName)
-		}
+		/*
+			for _, seat := range seatResp.Data.RemainSeat {
+				fmt.Printf("PlaySeq: %s, RemainCnt: %d, SeatGrade: %s, SeatGradeName: %s\n", seat.PlaySeq, seat.RemainCnt, seat.SeatGrade, seat.SeatGradeName)
+			}
+		*/
+
+		seatData = append(seatData, &seatResp.Data)
 	}
 
+	return seatData, nil
 }
