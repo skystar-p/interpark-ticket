e9b065ccacaf54f6ab2af8bf51f2ddbaa79ff7be
Jaehyeon Park <skystar@skystar.dev>
2024-08-07 13:32:10 +0900

    add ticketlink

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"sync"
 	"time"
 
 	"github.com/caarlos0/env/v11"
@@ -14,7 +15,8 @@ import (
 )
 
 var (
-	URL = "https://api-ticketfront.interpark.com/v1/goods/%s/playSeq/PlaySeq/%03d/REMAINSEAT"
+	InterparkURL  = "https://api-ticketfront.interpark.com/v1/goods/%s/playSeq/PlaySeq/%03d/REMAINSEAT"
+	TicketLinkURL = "https://mapi.ticketlink.co.kr/mapi/schedule/%s/grades?productClassCode=CONCERT&productId=%s"
 )
 
 var (
@@ -22,29 +24,42 @@ var (
 )
 
 type ConfigStruct struct {
-	GoodsID      string `env:"GOODS_ID,required"`
-	PlaySeqCount int    `env:"PLAY_SEQ_COUNT" envDefault:"1"`
+	InterparkGoodsID      string `env:"INTERPARK_GOODS_ID,required"`
+	InterparkPlaySeqCount int    `env:"INTERPARK_PLAY_SEQ_COUNT" envDefault:"1"`
+
+	TicketLinkProductId   string   `env:"TICKETLINK_PRODUCT_ID,required"`
+	TicketLinkScheduleIds []string `env:"TICKETLINK_SCHEDULE_IDS,required" envSeparator:","`
 
 	TelegramToken   string  `env:"TELEGRAM_TOKEN,required"`
 	TelegramChatIds []int64 `env:"TELEGRAM_CHAT_IDS,required" envSeparator:","`
 
 	SleepDuration time.Duration `env:"SLEEP_DURATION" envDefault:"5s"`
-	RenotifyAfter time.Duration `env:"RENOTIFY_AFTER" envDefault:"1m"`
+	RenotifyAfter time.Duration `env:"RENOTIFY_AFTER" envDefault:"30s"`
 }
 
-type SeatResponse struct {
-	Data SeatData `json:"data"`
+type InterparkSeatResponse struct {
+	Data InterparkSeatData `json:"data"`
 }
 
-type SeatData struct {
-	RemainSeat []Seat `json:"remainSeat"`
+type InterparkSeatData struct {
+	RemainSeat []InterparkSeat `json:"remainSeat"`
 }
 
-type Seat struct {
+type InterparkSeat struct {
 	PlaySeq       string `json:"playSeq"`
 	RemainCnt     int    `json:"remainCnt"`
 	SeatGrade     string `json:"seatGrade"`
 	SeatGradeName string `json:"seatGradeName"`
+}
+
+type TicketLinkSeatResponse struct {
+	SeatData []TicketLinkSeat `json:"data"`
+}
+
+type TicketLinkSeat struct {
+	Name       string `json:"name"`
+	RemainCnt  int    `json:"remainCnt"`
+	ScheduleID string `json:"scheduleId,omitempty`
 }
 
 func main() {
@@ -60,54 +75,92 @@ func main() {
 
 	log.Printf("Start checking...\n")
 
-	for _, chatId := range config.TelegramChatIds {
-		if _, err := bot.SendMessage(tu.Message(tu.ID(chatId), "Start checking...")); err != nil {
-			log.Printf("Failed to send message: %v\n", err)
-		}
+	// only send to first chat id
+	if _, err := bot.SendMessage(tu.Message(tu.ID(config.TelegramChatIds[0]), "Start checking...")); err != nil {
+		log.Printf("Failed to send message: %v\n", err)
 	}
 
-	seatDataCache := make(map[Seat]time.Time)
+	interparkSeatDataCache := make(map[InterparkSeat]time.Time)
+	ticketLinkSeatDataCache := make(map[TicketLinkSeat]time.Time)
 	for {
-		seatData, err := checkSeat()
-		if err != nil {
-			log.Printf("Failed to check seat: %v\n", err)
-			time.Sleep(config.SleepDuration)
-			continue
-		}
+		var wg sync.WaitGroup
+		wg.Add(2)
+		// interpark
+		go func() {
+			defer wg.Done()
+			log.Printf("Checking interpark...\n")
+			seatData, err := checkInterparkSeat()
+			if err != nil {
+				log.Printf("Failed to check seat: %v\n", err)
+				return
+			}
 
-		for _, seat := range seatData {
-			for _, s := range seat.RemainSeat {
-				now := time.Now()
-				if t, ok := seatDataCache[s]; ok {
-					if now.Sub(t) < config.RenotifyAfter {
-						continue
+			noAlarm := false
+			for _, seat := range seatData {
+				for _, s := range seat.RemainSeat {
+					now := time.Now()
+					if t, ok := interparkSeatDataCache[s]; ok {
+						if now.Sub(t) < config.RenotifyAfter {
+							noAlarm = true
+						} else {
+							interparkSeatDataCache[s] = now
+						}
 					} else {
-						seatDataCache[s] = now
+						interparkSeatDataCache[s] = now
 					}
-				} else {
-					seatDataCache[s] = now
-				}
-				if s.RemainCnt > 0 {
-					msg := fmt.Sprintf("Seat Found!!!: PlaySeq: %s, RemainCnt: %d, SeatGradeName: %s\n", s.PlaySeq, s.RemainCnt, s.SeatGradeName)
-					for _, chatId := range config.TelegramChatIds {
-						if _, err := bot.SendMessage(tu.Message(tu.ID(chatId), msg)); err != nil {
+					if s.RemainCnt > 0 && !noAlarm {
+						msg := fmt.Sprintf("<Interpark> Seat Found!!!: PlaySeq: %s, RemainCnt: %d, SeatGradeName: %s\n", s.PlaySeq, s.RemainCnt, s.SeatGradeName)
+						if err := sendTelegramMessage(bot, msg); err != nil {
 							log.Printf("Failed to send message: %v\n", err)
 						}
 					}
+					log.Printf("<Interpark> PlaySeq: %s, RemainCnt: %d, SeatGrade: %s, SeatGradeName: %s\n", s.PlaySeq, s.RemainCnt, s.SeatGrade, s.SeatGradeName)
 				}
-				log.Printf("PlaySeq: %s, RemainCnt: %d, SeatGrade: %s, SeatGradeName: %s\n", s.PlaySeq, s.RemainCnt, s.SeatGrade, s.SeatGradeName)
 			}
-		}
+		}()
+		// ticketlink
+		go func() {
+			defer wg.Done()
+			log.Printf("Checking ticketlink...\n")
+			seats, err := checkTicketLinkSeat()
+			if err != nil {
+				log.Printf("Failed to check seat: %v\n", err)
+				return
+			}
+
+			noAlarm := false
+			for _, seat := range seats {
+				now := time.Now()
+				if t, ok := ticketLinkSeatDataCache[*seat]; ok {
+					if now.Sub(t) < config.RenotifyAfter {
+						noAlarm = true
+					} else {
+						ticketLinkSeatDataCache[*seat] = now
+					}
+				} else {
+					ticketLinkSeatDataCache[*seat] = now
+				}
+				if seat.RemainCnt > 0 && !noAlarm {
+					msg := fmt.Sprintf("<TicketLink> Seat Found!!!: Name: %s, RemainCnt: %d, ScheduleID: %s\n", seat.Name, seat.RemainCnt, seat.ScheduleID)
+					if err := sendTelegramMessage(bot, msg); err != nil {
+						log.Printf("Failed to send message: %v\n", err)
+					}
+				}
+				log.Printf("<TicketLink> Name: %s, RemainCnt: %d, ScheduleID: %s\n", seat.Name, seat.RemainCnt, seat.ScheduleID)
+			}
+		}()
+
+		wg.Wait()
 
 		time.Sleep(config.SleepDuration)
 	}
 
 }
 
-func checkSeat() ([]*SeatData, error) {
-	var seatData []*SeatData
-	for i := 1; i <= config.PlaySeqCount; i++ {
-		urlStr := fmt.Sprintf(URL, config.GoodsID, i)
+func checkInterparkSeat() ([]*InterparkSeatData, error) {
+	var seatData []*InterparkSeatData
+	for i := 1; i <= config.InterparkPlaySeqCount; i++ {
+		urlStr := fmt.Sprintf(InterparkURL, config.InterparkGoodsID, i)
 		u, err := url.Parse(urlStr)
 		if err != nil {
 			return nil, err
@@ -133,7 +186,7 @@ func checkSeat() ([]*SeatData, error) {
 			return nil, err
 		}
 
-		var seatResp SeatResponse
+		var seatResp InterparkSeatResponse
 		if err := json.NewDecoder(resp.Body).Decode(&seatResp); err != nil {
 			return nil, err
 		}
@@ -142,4 +195,56 @@ func checkSeat() ([]*SeatData, error) {
 	}
 
 	return seatData, nil
+}
+
+func checkTicketLinkSeat() ([]*TicketLinkSeat, error) {
+	var seats []*TicketLinkSeat
+	for _, scheduleID := range config.TicketLinkScheduleIds {
+		urlStr := fmt.Sprintf(TicketLinkURL, scheduleID, config.TicketLinkProductId)
+		u, err := url.Parse(urlStr)
+		if err != nil {
+			return nil, err
+		}
+
+		headers := http.Header{
+			"User-Agent":      []string{"Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0"},
+			"Accept":          []string{"application/json, text/plain, */*"},
+			"Accept-Language": []string{"ko-KR,en-US;q=0.7,en;q=0.3"},
+			"Accept-Encoding": []string{"gzip, deflate, br, zstd"},
+			"Origin":          []string{"https://www.ticketlink.co.kr"},
+		}
+
+		req := &http.Request{
+			Method: "GET",
+			URL:    u,
+			Header: headers,
+		}
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			return nil, err
+		}
+
+		var seatResp TicketLinkSeatResponse
+		if err := json.NewDecoder(resp.Body).Decode(&seatResp); err != nil {
+			return nil, err
+		}
+
+		for _, seat := range seatResp.SeatData {
+			seat.ScheduleID = scheduleID
+			seats = append(seats, &seat)
+		}
+	}
+
+	return seats, nil
+}
+
+func sendTelegramMessage(bot *telego.Bot, msg string) error {
+	for _, chatID := range config.TelegramChatIds {
+		if _, err := bot.SendMessage(tu.Message(tu.ID(chatID), msg)); err != nil {
+			return err
+		}
+	}
+	return nil
 }
